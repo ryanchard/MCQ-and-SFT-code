d00904b50451ac1933cd7649508fa26f3f9c1015
Ian Foster <foster@anl.gov>
2025-01-18 17:18:28 -0600

    Create check_alcf_service_status.py

---
+++ check_alcf_service_status.py
@@ -0,0 +1,40 @@
+import requests
+
+with open('alcf_access_token.txt', 'r') as file:
+    alcf_access_token = file.read().strip()
+
+# Define the URL and headers
+url = "https://data-portal-dev.cels.anl.gov/resource_server/sophia/jobs"
+headers = {
+    "Authorization": f"Bearer {alcf_access_token}"
+}
+
+# Make the GET request
+response = requests.get(url, headers=headers)
+
+# Check the response
+if response.status_code == 200:
+    status = response.json()
+else:
+    print("Error:", response.status_code, response.text)
+    exit(1)
+
+#running = status['running']
+#for run in running:
+    # print(run)
+
+def get_models(status, status1, status2):
+    models = status[status1]
+    model_list = []
+    for model in models:
+        model_list += model['Models Served'].split(',')
+        if model['Model Status'] != status2:
+            print(f'SURPRISE: Expected {status2} but got {model["Model Status"]} in {model}')
+    return model_list
+
+models_running = get_models(status, 'running', 'running')
+print(f'Running: {models_running}')
+
+models_queued = get_models(status, 'queued', 'starting')
+print(f'Queued : {models_queued}')
+
