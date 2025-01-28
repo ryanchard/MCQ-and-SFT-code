0ec2e746d14aebce0928fe8b27abddc8f8a68e6a
Ian Foster <foster@polaris-login-04.hsn.cm.polaris.alcf.anl.gov>
2025-01-28 22:11:23 +0000

    update

--- check_alcf_service_status.py
+++ check_alcf_service_status.py
@@ -3,6 +3,9 @@ import sys
 
 from inference_auth_token import get_access_token
 alcf_access_token = get_access_token()
+from alcf_inference_utilities import get_names_of_alcf_chat_models, get_alcf_inference_service_model_queues
+alcf_chat_models = get_names_of_alcf_chat_models(alcf_access_token)
+
 
 # Define the URL and headers
 url = "https://data-portal-dev.cels.anl.gov/resource_server/sophia/jobs"
@@ -32,10 +35,13 @@ def get_models(status, status1, status2):
     return model_list
 
 models_running = get_models(status, 'running', 'running')
-print(f'Running: {models_running}')
+print(f'Running : {models_running}')
 
 models_starting = get_models(status, 'running', 'starting')
 print(f'Starting: {models_starting}')
 
 models_queued = get_models(status, 'queued', 'starting')
-print(f'Queued : {models_queued}')
+print(f'Queued  : {models_queued}')
+
+other_models = [model for model in alcf_chat_models if model not in models_queued and model not in models_starting and model not in models_running]
+print(f'Inactive: {other_models}')
